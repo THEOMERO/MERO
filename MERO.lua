ca0ac19666a82080c210a6c1e4f4018474457405
THEOMERO <68134325+THEOMERO@users.noreply.github.com>
2020-11-02 20:46:03 +0300

    Add files via upload

--- MERO.lua
+++ MERO.lua
@@ -2484,7 +2484,7 @@ end,nil)
 elseif text == 'قفل الفارسيه' and msg.reply_to_message_id_ == 0 and Manager(msg) then 
 database:set(bot_id..'lock:Fars'..msg.chat_id_,true) 
 tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,data) 
-send(msg.chat_id_, msg.id_,'*⋄︙ بواسطه ↫*['..Rutba(msg.sender_user_id_,msg.chat_id_)..'](T.ME/'..(data.username_ or 'MERO170')..') \n*⋄︙ تم قفـل ↫ الفارسيه\n*⋄︙ الايدي ↫* `'..msg.sender_user_id_..'`')
+send(msg.chat_id_, msg.id_,'*⋄︙ بواسطه ↫*['..Rutba(msg.sender_user_id_,msg.chat_id_)..'](T.ME/'..(data.username_ or 'MERO170')..') \n*⋄︙ تم قفـل ↫ الفارسيه*\n*⋄︙ الايدي ↫* `'..msg.sender_user_id_..'`')
 end,nil)   
 elseif text == 'قفل الانلاين' and msg.reply_to_message_id_ == 0 and Manager(msg) then 
 database:set(bot_id.."lock:inline"..msg.chat_id_,'del')  
