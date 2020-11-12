28e4f8666afa5358dd8edae700fe6c05c9b738f9
THEOMERO <68134325+THEOMERO@users.noreply.github.com>
2020-11-12 12:07:38 +0300

    Add files via upload

--- MERO.lua
+++ MERO.lua
@@ -2145,7 +2145,7 @@ end
 tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(extra,result,success)
 tdcli_function({ID ="GetChat",chat_id_=msg.chat_id_},function(arg,chat)  
 if not database:sismember(bot_id..'Chek:Groups',msg.chat_id_) then
-send(msg.chat_id_, msg.id_,'⋄︙ المجموعه تم تطيلها من قبل')
+send(msg.chat_id_, msg.id_,'⋄︙ المجموعه تم تعطيلها من قبل')
 else
 sendText(msg.chat_id_,'\n*⋄︙ بواسطه ↫* ['..string.sub(result.first_name_,0, 70)..'](tg://user?id='..result.id_..')\n*⋄︙تم تعطيل المجموعه* '..chat.title_..'',msg.id_/2097152/0.5,'md')
 database:srem(bot_id..'Chek:Groups',msg.chat_id_)
