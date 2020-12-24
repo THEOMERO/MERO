76952faf77207f57a18331f2807874e00db1e774
THEOMERO <68134325+THEOMERO@users.noreply.github.com>
2020-12-24 20:02:29 +0300

    Add files via upload

--- MERO.lua
+++ MERO.lua
@@ -12059,7 +12059,7 @@ database:set(bot_id..":"..msg.sender_user_id_..":rjo_Bots"..msg.chat_id_,"sendre
 Text = '*ارسل اسم الشخص الذي تريد قياس نسبه انوثتها* \n *مثال تبارك*'
 send(msg.chat_id_, msg.id_,Text) 
 end
-if text and text ~="نسبه الانوثه" and database:get(bot_id..":"..msg.sender_user_id_..":rjo_Bots"..msg.chat_id_) == "sendreno" then
+if text and text ~="نسبه انوثه" and database:get(bot_id..":"..msg.sender_user_id_..":rjo_Bots"..msg.chat_id_) == "sendreno" then
 numj = {"10","20","30","35","75","34","66","82","23","19","55","8","63","32","27","89","99","98","3","3","8","3","6","0",};
 sendnuj = numj[math.random(#numj)]
 xl = 'نسبه انوثة '..text..' هي : \n '..sendnuj..'%'
